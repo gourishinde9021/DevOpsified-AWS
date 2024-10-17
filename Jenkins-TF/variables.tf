0b94b579ce9c1988cb87eff3e5e8b06fb0c7b412
GouriS <gourisshinde21@gmail.com>
2024-10-17 11:50:23 +0530

    Added files for Jenkins terraform automation

---
+++ Jenkins-TF/variables.tf
@@ -0,0 +1,24 @@
+variable "vpc-name" {
+    default = "Jenkins-vpc"
+}
+variable "igw-name" {
+    default = "Jenkins-igw"
+}
+variable "rt-name" {
+    default = "Jenkins-route-table"
+}
+variable "subnet-name" {
+    default = "Jenkins-subnet"
+}
+variable "sg-name" {
+    default = "Jenkins-sg"
+}
+variable "instance-name" {
+    default = "Jenkins-server"
+}
+variable "key-name" {
+    default = "Gouri-Shinde"
+}
+variable "iam-role" {
+    default = "Jenkins-iam-role"
+}
