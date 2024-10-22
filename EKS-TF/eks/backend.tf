8b5bab241db859e8d7c2fa32c02a16630508c929
GouriS <gourisshinde21@gmail.com>
2024-10-22 18:19:07 +0530

    updated backend.tf

--- EKS-TF/eks/backend.tf
+++ EKS-TF/eks/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "my-ews-bucket-39343094"
     region = "ap-south-1"
-    key = "DevOpsified-AWS/Jenkins-TF/terraform.tfstate"
+    key = "DevOpsified-AWS/EKS-TF/terraform.tfstate"
     encrypt = true
   }
 }
